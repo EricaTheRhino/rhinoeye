300a54d0c12838da80b131976af2eb3cd4c402d7
ss <ss@408bdfbd-0bd4-0410-88bf-c0b946b28f18>
2013-05-27 16:42:22 +0000

    fixed the servocontrol
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/ecsrhino/rhinoeye@70 408bdfbd-0bd4-0410-88bf-c0b946b28f18

--- src/main/java/rhino/ServoControl.java
+++ src/main/java/rhino/ServoControl.java
@@ -18,26 +18,38 @@ public class ServoControl implements PreferenceChangeListener{
 //	int SERVO_MAX=220;
 //	long SERVO_WAIT_TIME=50;
 	final String devLocation = "/dev/servoblaster";
-	int currentPWM;
 	int PWMdelta=5;
 	PrintWriter servoOut;
 	String servoID = "0";
-	boolean servoActive=true;
+	boolean servoActive=false;
 	private long lastServoCommand = 0;
 	private float pwmProp;
+	private int controlToken;
 	private ServoControl(){
-		if(!new File(devLocation).exists()){
-			servoActive=false;
-		}
-		try {
-			servoOut = new PrintWriter(new FileOutputStream(devLocation));
-		} catch (FileNotFoundException e) {
-
-		}
+		initServoOut();
 		reinit();
 		RhinoPrefs.getPrefs().addPreferenceChangeListener(this);
 	}
 	
+	private void initServoOut() {
+		// If the file does not exist deactivate the servo
+		if(!new File(devLocation).exists()){
+			servoActive = false;
+			servoOut = null;
+		}
+		else{			
+			// If the servo is not currently active, try to activate it
+			if(!servoActive){			
+				try {
+					servoOut = new PrintWriter(new FileOutputStream(devLocation));
+					servoActive = true;
+				} catch (FileNotFoundException e) {
+					servoActive=false;
+				}
+			}
+		}
+	}
+
 	public int SERVO_MIN(){return RhinoPrefs.getInt("eye.servo.min",40);}
 	public int SERVO_MAX(){return RhinoPrefs.getInt("eye.servo.max",220);}
 	public long SERVO_WAIT_TIME(){return RhinoPrefs.getLong("eye.servo.wait",50l);}
@@ -46,42 +58,49 @@ public class ServoControl implements PreferenceChangeListener{
 		if(servoInstance == null) servoInstance = new ServoControl();
 		return servoInstance;
 	}
+	
+	public synchronized int requestControlToken(){
+		return ++this.controlToken;
+	}
+	public synchronized boolean confirmControl(int token){
+		return this.controlToken == token;
+	}
 	public void servoSet(float pwmProp){
 		if(pwmProp>1f)pwmProp = 1f;
 		if(pwmProp<0f)pwmProp=0f;
-		servoSet((int)(SERVO_MIN() + DELTA() * pwmProp));
+		this.pwmProp = pwmProp;
+		servoSet(requestControlToken(),(int)(SERVO_MIN() + DELTA() * pwmProp));
+	}
+	private void servoSet(int controlToken,float pwmProp){
+		if(pwmProp>1f)pwmProp = 1f;
+		if(pwmProp<0f)pwmProp=0f;
+		this.pwmProp = pwmProp;
+		servoSet(controlToken,(int)(SERVO_MIN() + DELTA() * pwmProp));
 	}
 	public void servoSet(float pwmProp,long interval){
-		if(pwmProp>1f)pwmProp = 1f;
+		int token = this.requestControlToken();
+		if(pwmProp>1f)pwmProp=1f;
 		if(pwmProp<0f)pwmProp=0f;
 		logging.info("Current prop is: " + this.pwmProp);
 		logging.info("Target prop is: " + pwmProp);
+		float targetDelta = pwmProp - this.pwmProp;
 		long steps = interval/SERVO_WAIT_TIME();
 		logging.info("N steps: " + steps);
-		int incr = (int) (( (SERVO_MIN() + pwmProp * DELTA())- this.currentPWM )/steps);
+		float incr = targetDelta / steps;
 		logging.info("Increment: " + incr);
-		// Min move is 1
-		if(incr == 0)
-			if(pwmProp > this.pwmProp) 
-				incr = -1;
-			else if (pwmProp < this.pwmProp)
-				incr = 1;
-			else
-			{
-				incr=0;
-				steps=0;
-			}
-		
+		if(incr == 0) return;
 		for (int i = 0; i < steps; i++) {
-			this.servoSet(this.currentPWM + incr);
+			this.servoSet(token,this.pwmProp + incr);
 		}
+		this.pwmProp = pwmProp;
+		this.servoSet(token,this.pwmProp);
 		
 	}
-	public void servoSet(int newPWM) {
+	private void servoSet(int controlToken, int newPWM) {
+		if(!confirmControl(controlToken))return;
+		initServoOut();
 		logging.info("Attemping to set PWM to: " + newPWM);
 		newPWM = Math.max(Math.min(newPWM, SERVO_MAX()),SERVO_MIN());
-		currentPWM = newPWM;
-		this.pwmProp = getDirection();
 		if(servoActive){			
 			if(System.currentTimeMillis() - lastServoCommand <= SERVO_WAIT_TIME()) return;
 			String servoMessage = String.format("%s=%s",servoID ,newPWM);
@@ -90,19 +109,25 @@ public class ServoControl implements PreferenceChangeListener{
 			servoOut.flush();
 			this.lastServoCommand  = System.currentTimeMillis();
 		}
+		else{
+			logging.error("NO SERVO ACTIVE");
+			logging.error("setting fake pwmPROP to: " + this.pwmProp + " with PWM: " + newPWM);
+		}
 		try {
+			
 			logging.info("Sleeping: " +SERVO_WAIT_TIME());
 			Thread.sleep(SERVO_WAIT_TIME());
 		} catch (InterruptedException e) {
 		}
 	}
-	public void servoDelta(int delta) {
-		servoSet(currentPWM + delta);
-	}
+	
 	public void servoDelta(float delta) {
-		servoSet(currentPWM + (int)(delta * DELTA()));
+		this.pwmProp += delta;
+		int newPWM = (int) (SERVO_MIN() + DELTA() * this.pwmProp);
+		this.servoSet(requestControlToken(),newPWM);
 	}
 	public void reinit() {
+		this.pwmProp = 0.5f;
 		servoSet(SERVO_MIN() + (DELTA()/2));
 	}
 
@@ -111,7 +136,7 @@ public class ServoControl implements PreferenceChangeListener{
 	}
 
 	public float getDirection() {
-		return (this.currentPWM-SERVO_MIN()) / (float)DELTA();
+		return this.pwmProp;
 	}
 
 	@Override
